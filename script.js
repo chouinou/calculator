0ccb24451d79cca17c226c8f8d2bfb3630bc87f5
chouinou <311111800+chouinou@users.noreply.github.com>
2026-09-04 21:51:51 +0200

    Update html, css and js

--- script.js
+++ script.js
@@ -2,9 +2,17 @@
 // <----------- Variables ----------->
 let firstOperand = ""
 let secondOperand = ""
+let currentOperation = null
 let operationType = null
 
-const screenDisplay = document.getElementById('currentOperationScreen')
+const lastOperationScreen = document.getElementById("lastOperationScreen")
+const currentOperationScreen = document.getElementById("currentOperationScreen")
+
+
+// Buttons
+const numberButtons = document.querySelectorAll('#number')
+const operatorButtons = document.querySelectorAll('#operator')
+
 const clearButton = document.getElementById('clear')
 const equalButton = document.getElementById('equal')
 const plusButton = document.getElementById('plus')
@@ -12,9 +20,46 @@ const minusButton = document.getElementById('minus')
 const multiplyButton = document.getElementById('multiply')
 const divideButton = document.getElementById('divide')
 
-
-
 // <----------- Functions ----------->
+function appendNumber(number) {
+    currentOperationScreen.textContent += number ;
+}
+
+function evaluate(a, b) {
+  a = Number(a)
+  b = Number(b)
+  switch (operationType) {
+    case '+':
+      return add(a, b)
+    case '−':
+      return substract(a, b)
+    case '×':
+      return multiply(a, b)
+    case '÷':
+      if (b === 0) return null
+      else return divide(a, b)
+    default:
+      return null
+}}
+
+function clear() {
+    currentOperationScreen.textContent = '' ;
+    lastOperationScreen.textContent = '' ;
+    operationType = null ;
+    firstOperand = '' ;
+    secondOperand = '' ;
+}
+
+function setOperation(type) {
+    // // Doing the math
+    if (operationType !== null) evaluate() ;
+    // data saving
+    firstOperand = currentOperationScreen.textContent ;
+    currentOperationScreen.textContent = null ;
+    operationType = type ;
+    lastOperationScreen.textContent = `${firstOperand} ${operationType}` ;
+}
+
 function add(a,b) {
     return a + b ;
 }
@@ -31,30 +76,14 @@ function divide(a,b) {
     return a / b ;
 }
 
-// function evaluate(a, b) {
-//   a = Number(a)
-//   b = Number(b)
-//   switch (operationType) {
-//     case '+':
-//       return add(a, b)
-//     case '−':
-//       return substract(a, b)
-//     case '×':
-//       return multiply(a, b)
-//     case '÷':
-//       if (b === 0) return null
-//       else return divide(a, b)
-//     default:
-//       return null
-// }}
-
-function clearScreen() {
-}
-
-function chooseOperation(type) {
-    operationType = type
-    console.log(operationType) ;
-}
-
 // <----------- Events listeners ----------->
-clearButton.addEventListener('click', clearScreen)
+clearButton.addEventListener('click', clear)
+
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent)))
+
+operatorButtons.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent)))
+
+
+
